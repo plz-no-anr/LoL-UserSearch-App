ec26375e6870ce7fddb68ddedbe54a844f2d4e9b
plz-no-anr <psgxxx@naver.com>
2023-02-13 00:42:40 +0900

    1. Timber 및 Custom DebugTree 추가

--- app/src/main/java/com/plznoanr/lol_usersearch_app/LolApplication.kt
+++ app/src/main/java/com/plznoanr/lol_usersearch_app/LolApplication.kt
@@ -1,9 +1,17 @@
 package com.plznoanr.lol_usersearch_app
 
 import android.app.Application
+import com.plznoanr.lol_usersearch_app.utils.TimberDebugTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class LolApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(TimberDebugTree())
+        }
+    }
 
 }
