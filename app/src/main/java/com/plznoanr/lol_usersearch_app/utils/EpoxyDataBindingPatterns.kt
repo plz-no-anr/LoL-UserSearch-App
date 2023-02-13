ed03d9fdade7611f95471472215a99e4d26e693f
plz-no-anr <psgxxx@naver.com>
2023-02-14 00:52:16 +0900

    1. Epoxy 추가

---
+++ app/src/main/java/com/plznoanr/lol_usersearch_app/utils/EpoxyDataBindingPatterns.kt
@@ -0,0 +1,6 @@
+package com.plznoanr.lol_usersearch_app.utils
+import com.airbnb.epoxy.EpoxyDataBindingPattern
+import com.plznoanr.lol_usersearch_app.R
+
+@EpoxyDataBindingPattern(rClass = R::class, layoutPrefix = "epoxy")
+object EpoxyDataBindingPatterns
