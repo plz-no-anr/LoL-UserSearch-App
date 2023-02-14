78843ea3b272ad41e6b20db73f4298b3849622dd
plz-no-anr <psgxxx@naver.com>
2023-02-14 21:33:35 +0900

    1. Base 파일 추가

--- app/src/main/java/com/plznoanr/lol_usersearch_app/ui/base/BaseFragment.kt
+++ app/src/main/java/com/plznoanr/lol_usersearch_app/ui/base/BaseFragment.kt
@@ -1,13 +1,23 @@
 package com.plznoanr.lol_usersearch_app.ui.base
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.plznoanr.lol_usersearch_app.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-open class BaseFragment: Fragment() {
+abstract class BaseFragment: Fragment() {
 
     open fun navigateTo(actionId: Int) {
         findNavController().navigate(actionId)
     }
+
+    open fun initViews() {}
+
+    open fun initObservers() {}
 
 }
